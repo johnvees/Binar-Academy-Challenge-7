8d0ba994572b22ecde74c3e83dbe70cc3c95e416
johnvees <yohanesvelly23@gmail.com>
2022-05-24 12:29:43 +0700

    add firebase configs

---
+++ src/configs/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB27mlf-1EhBKf80GfSKDLiRRSj3dBHE3c',
+  authDomain: 'chatapp-191b3.firebaseapp.com',
+  projectId: 'chatapp-191b3',
+  storageBucket: 'chatapp-191b3.appspot.com',
+  messagingSenderId: '866544463746',
+  appId: '1:866544463746:web:becfcce1f90fb85f5f299b',
+  measurementId: 'G-B3ZV5LB0DB',
+});
+
+const Fire = firebase;
+
+export default Fire;
